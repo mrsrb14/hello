21b4ded7c5f3eb027eed51c2dcb108e2ec4cbd0f
Sourabh <srbking1111@gmail.com>
2022-07-01 16:50:49 +0530

    first commit

---
+++ computer.py
@@ -0,0 +1,357 @@
+def intel_i3():
+    choose = int(input("""please select the option of your processor generation"))
+    1 for 2nd generation
+    2 for 5th generation
+    3 for 11th generation"""))
+    #2nd generation
+    if choose==1:
+        company = int(input("""please select the option for perticular company which is mention below")))
+        1 for gegabyte 
+        2 for asus
+        3 for zebronics"""))
+
+        #gegabyte
+        if company==1:
+            print("""you can select
+            intel H61
+            """) 
+        #asus
+        elif company==2:
+            print("""you can select
+            LGA H61
+            """) 
+        #zebronics
+        elif company==3:
+            print("""you can select
+            Zeb H61
+            """) 
+        else:
+            print("please select the corrent option")
+
+    #5th generation
+    elif choose==2:
+        company = int(input("""please select the option for perticular company which is mention below")))
+        1 for gegabyte 
+        2 for asus
+        3 for zebronics"""))
+        #gegabte
+        if company==1:
+            print("""you can select
+            zeb H61
+            """) 
+        #asus
+        elif company==2:
+            print("""you can select
+            H61-0M
+            """) 
+        #zebronics
+        elif company==3:
+            print("""you can select
+           Zeb H61
+            """) 
+        else:
+            print("please select the corrent option")
+
+    #11th generation
+    elif choose==3:
+
+        company = int(input("""please select the option for perticular company which is mention below")))
+        1 for gegabyte 
+        2 for asus
+        3 for zebronics"""))
+    #gegabyte
+        if company==1:
+            print("""you can select
+           H510M-H
+            """) 
+
+    #asus
+        elif company==2:
+            print("""you can select
+            H510M-H
+            """) 
+
+    #zebronics
+        elif company==3:
+            print("""you can select
+            H510M-A
+            """) 
+        else:
+            print("please select the corrent option")
+    else:
+        print("please select the corrent option")
+
+def intel_i5():
+    choose = int(input("""please select the option of your processor generation"))
+    1 for 2nd generation
+    2 for 5th generation
+    3 for 11th generation"""))
+    #2nd generation
+    if choose==1:
+        company = int(input("""please select the option for perticular company which is mention below")))
+        1 for gegabyte 
+        2 for asus
+        3 for zebronics"""))
+        #gegabyte
+        if company==1:
+            print("""you can select
+            intel H410 M-H
+            """) 
+        #asus
+        elif company==2:
+            print("""you can select
+            B450 MA
+            """) 
+        #zebronics
+        elif company==3:
+            print("""you can select
+            Zeb H61
+            """) 
+        else:
+            print("please select the corrent option")
+
+    #5th generation
+    elif choose==2:
+        company = int(input("""please select the option for perticular company which is mention below")))
+        1 for gegabyte 
+        2 for asus
+        3 for zebronics"""))
+        #gegabte
+        if company==1:
+            print("""you can select
+            zeb H61
+            """) 
+        #asus
+        elif company==2:
+            print("""you can select
+            H61-0M
+            """) 
+        #zebronics
+        elif company==3:
+            print("""you can select
+           Zeb H61
+            """) 
+        else:
+            print("please select the corrent option")
+
+    #11th generation
+    elif choose==3:
+
+        company = int(input("""please select the option for perticular company which is mention below")))
+        1 for gegabyte 
+        2 for asus
+        3 for zebronics"""))
+    #gegabyte
+        if company==1:
+            print("""you can select
+           H510M-H
+            """) 
+
+    #asus
+        elif company==2:
+            print("""you can select
+            H510M-H
+            """) 
+
+    #zebronics
+        elif company==3:
+            print("""you can select
+            H510M-A
+            """) 
+        else:
+            print("please select the corrent option")
+    else:
+        print("please select the corrent option")
+
+def intel_i7():
+    choose = int(input("""please select the option of your processor generation"))
+    1 for 2nd generation
+    2 for 5th generation
+    3 for 11th generation"""))
+    #2nd generation
+    if choose==1:
+        company = int(input("""please select the option for perticular company which is mention below")))
+        1 for gegabyte 
+        2 for asus
+        3 for zebronics"""))
+        #gegabyte
+        if company==1:
+            print("""you can select
+            intel H61
+            """) 
+        #asus
+        elif company==2:
+            print("""you can select
+            LGA H61
+            """) 
+        #zebronics
+        elif company==3:
+            print("""you can select
+            Zeb H61
+            """) 
+        else:
+            print("please select the corrent option")
+
+    #5th generation
+    elif choose==2:
+        company = int(input("""please select the option for perticular company which is mention below")))
+        1 for gegabyte 
+        2 for asus
+        3 for zebronics"""))
+        #gegabte
+        if company==1:
+            print("""you can select
+        	Intel H81 Chipset
+            """) 
+        #asus
+        elif company==2:
+            print("""you can select
+            H81-0M
+            """) 
+        #zebronics
+        elif company==3:
+            print("""you can select
+           Zeb Intel H81 Chipset
+            """) 
+        else:
+            print("please select the corrent option")
+
+    #11th generation
+    elif choose==3:
+
+        company = int(input("""please select the option for perticular company which is mention below")))
+        1 for gegabyte 
+        2 for asus
+        3 for zebronics"""))
+    #gegabyte
+        if company==1:
+            print("""you can select
+           GIGABYTE B450 AORUS PRO
+            """) 
+
+    #asus
+        elif company==2:
+            print("""you can select
+            B450
+            """) 
+
+    #zebronics
+        elif company==3:
+            print("""you can select
+            B450
+            """) 
+        else:
+            print("please select the corrent option")
+    else:
+        print("please select the corrent option")
+
+def intel_i9():
+    choose = int(input("""please select the option of your processor generation"))
+    1 for 2nd generation
+    2 for 5th generation
+    3 for 11th generation"""))
+    #2nd generation
+    if choose==1:
+        company = int(input("""please select the option for perticular company which is mention below")))
+        1 for gegabyte 
+        2 for asus
+        3 for zebronics"""))
+        #gegabyte
+        if company==1:
+            print("""you can select
+            not available
+            """) 
+        #asus
+        elif company==2:
+            print("""you can select
+            not available
+            """) 
+        #zebronics
+        elif company==3:
+            print("""you can select
+            not available
+            """) 
+        else:
+            print("please select the corrent option")
+
+    #5th generation
+    elif choose==2:
+        company = int(input("""please select the option for perticular company which is mention below")))
+        1 for gegabyte 
+        2 for asus
+        3 for zebronics"""))
+        #gegabte
+        if company==1:
+            print("""you can select
+            not available
+            """) 
+        #asus
+        elif company==2:
+            print("""you can select
+            not available
+            """) 
+        #zebronics
+        elif company==3:
+            print("""you can select
+           not available
+            """) 
+        else:
+            print("please select the corrent option")
+
+    #11th generation
+    elif choose==3:
+
+        company = int(input("""please select the option for perticular company which is mention below")))
+        1 for gegabyte 
+        2 for asus
+        3 for zebronics"""))
+    #gegabyte
+        if company==1:
+            print("""you can select
+           ZEB 590 UD
+            """) 
+
+    #asus
+        elif company==2:
+            print("""you can select
+            ASUS prime Z590P
+            """) 
+
+    #zebronics
+        elif company==3:
+            print("""you can select
+            Zeb 590
+            """) 
+        else:
+            print("please select the corrent option")
+    else:
+        print("please select the corrent option")
+
+while True:
+    run = str(input("press any key to start the program or q for quit:"))
+    if run=="Q" or run=="q":
+        print("thank you for using this program")
+        break
+    else:
+        try:
+            choose = int(input(
+                """"enter the option
+                1 for i3
+                2 for i5
+                3 for i7
+                4 for i9 """))
+            if choose==1:
+                intel_i3()
+            elif choose==2:
+                intel_i5()
+            elif choose==3:
+                intel_i7()
+            elif choose==4:
+                intel_i9()
+            else:
+                print("choose the correct option")
+        except:
+            print("choose the correct version of processor")
+        
+
+
